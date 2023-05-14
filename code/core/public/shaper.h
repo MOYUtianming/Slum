6ce5456418d78037743ec18c9ad8ff2f3deaf847
shyy <mccflv@gmail.com>
2023-05-14 23:37:25 +0800

    phasor

--- code/core/public/shaper.h
+++ code/core/public/shaper.h
@@ -1,30 +1,35 @@
 #ifndef __SHAPER_H__
 #define __SHAPER_H__
 
-namespace audio_signal_generator {
-    class shaper
+namespace AudioSignalGenerator {
+    class Shaper
     {
     public:
         virtual double apply_shape(double phase) = 0;
     };
 
-    class sin_shaper: public shaper
+    constexpr unsigned int TAYLOR_ITER_NUM_LIM = 1000;
+    class SinShaper: public Shaper
+    {
+    private:
+        unsigned int taylor_iter_num;
+    public:
+        SinShaper();
+        SinShaper(unsigned int _taylor_iter_num_);
+        double apply_shape(double phase);
+    };
+
+    class SqrShaper: public Shaper
     {
     public:
         double apply_shape(double phase);
     };
 
-    class sqr_shaper: public shaper
+    class TriShaper: public Shaper
     {
     public:
         double apply_shape(double phase);
     };
-
-    class tri_shaper: public shaper
-    {
-    public:
-        double apply_shape(double phase);
-    };
-} // namespace audio_signal_generator
+} // namespace AudioSignalGenerator
 
 #endif
--- code/core/source/timer.cpp
+++ code/core/source/timer.cpp
@@ -1,14 +1,16 @@
 #include "timer.h"
+#include "f_cmp.h"
 #include <cmath>
+#include <iostream>
 
-namespace audio_signal_generator {
+namespace AudioSignalGenerator {
     Timer::Timer(double _peroid_, double _step_, double _epsilon_, double _start_time_)
     {
         peroid  = _peroid_;
         step    = _step_;
         epsilon = _epsilon_;
-        curTime = _start_time_ - step; // for Iterator
-        timeLimit = peroid + step;
+        cur_time = _start_time_ - step; // for iterator logic
+        time_lim = peroid + step; // T + step // for iterator logic
     }
 
     Timer::~Timer()
@@ -17,10 +19,11 @@ namespace audio_signal_generator {
 
     double Timer::iterator_timer()
     {
-        curTime += step;
-        if(fabs(curTime - timeLimit) < epsilon) {
-            curTime = 0;
+        cur_time += step;
+        if(f_geq(cur_time, time_lim)) {
+            cur_time = 0;
         }
-        return curTime;
+        std::cout << "cur_time" << cur_time << std::endl;
+        return cur_time;
     }
-}  // namespace audio_signal_generator
+}  // namespace AudioSignalGenerator
