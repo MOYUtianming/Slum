b08ee9353d4a88f7203077e146aedb60b9e9e1e0
shyy <mccflv@gmail.com>
2023-07-03 21:32:23 +0800

    realize test_file_processor

--- case/case3_slum_test_file.cpp
+++ case/case3_slum_test_file.cpp
@@ -12,18 +12,17 @@ int test1_write_str()
 {
     std::string str_fn = "./_output/case3/str.log";
     Slum::TestFileProcessor tfp(str_fn, std::ios_base::out);
-    char hw[] = "hello world!";
-    tfp.write_as_char(hw, sizeof(hw));
+    char buf[] = "hello world!";
+    tfp.write_as_char(buf, sizeof(buf));
     return 0;
 }
 
 int test2_write_bin()
 {
     std::string str_fn = "./_output/case3/bin.bin";
-    std::ios_base::openmode opm = std::ios_base::out | std::ios_base::binary;
-    Slum::TestFileProcessor tfp(str_fn, opm);
-    char hw[] = "hello world!";
-    tfp.write_as_bin(hw, sizeof(hw));
+    Slum::TestFileProcessor tfp(str_fn, std::ios_base::out | std::ios_base::binary);
+    char buf[] = "hello world!";
+    tfp.write_as_bin(buf, sizeof(buf));
     return 0;
 
 }
@@ -40,7 +39,11 @@ int test3_read_str()
 
 int test4_read_bin()
 {
-
+    std::string str_fn = "./_output/case3/bin.bin";
+    Slum::TestFileProcessor tfp(str_fn, std::ios_base::in | std::ios_base::binary);
+    char buf[1024];
+    tfp.read_as_bin(buf, sizeof(buf));
+    std::cout << buf << std::endl;
     return 0;
 }
 
@@ -50,5 +53,6 @@ int main()
     test1_write_str();
     test2_write_bin();
     test3_read_str();
+    test4_read_bin();
     return 0;
 }
